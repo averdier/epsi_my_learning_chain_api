f78478198d13f7b6c1964f7e2fd8e4a6be29191d
Arthur Verdier <a.verdier@outlook.fr>
2018-04-12 18:51:41 +0200

    add files to project

--- app/api/serializers/projects.py
+++ app/api/serializers/projects.py
@@ -1,14 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from flask_restplus import fields
+from .nested import file_nested
 from .. import api
 
 
-project_resource_model = api.model('Project resource model', {
+project_minimal_model = api.model('Project minimal model', {
     'id': fields.String(required=True, description='Project ID'),
     'name': fields.String(required=True, description='Project name')
 })
 
+project_resource_model = api.inherit('Project resource model', project_minimal_model, {
+    'files': fields.List(fields.Nested(file_nested), required=True, description='Files list')
+})
+
 project_container = api.model('Project container', {
-    'projects': fields.List(fields.Nested(project_resource_model), required=True, description='Projects list')
+    'projects': fields.List(fields.Nested(project_minimal_model), required=True, description='Projects list')
 })
